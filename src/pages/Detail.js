8bc6ced8e4a0f441548e83ed0863636992109c4b
maeda <hiro92196@gmail.com>
2020-12-30 12:38:30 +0900

    detail

--- src/pages/Detail.js
+++ src/pages/Detail.js
@@ -15,76 +15,55 @@ import {
     CInputGroupPrepend,
     CInputGroupText,
     CInput,
-    CBadge
+    CBadge,
+    CCarousel,
+    CCarouselIndicators,
+    CCarouselInner,
+    CCarouselItem,
+    CCarouselCaption,
+    CCarouselControl
 } from '@coreui/react'
+// import {
+// } from '@coreui/coreui'
 
 import '../style.scss';
 
-const INITIAL_TASK = [
-    {id: 1, title: 'Reactのお勉強',doing: false},
-    {id: 2, title: '買い物',doing: false},
-    {id: 3, title: '掃除',doing: false},
-    {id: 4, title: '読書',doing: false},
-    {id: 5, title: '書類整理',doing: false},
-    {id: 6, title: 'テスト',doing: false}
-]
-
 export const Detail = () => {
-    const [tasks, setTasks] = useState(INITIAL_TASK)
-    const [task_title, setTask_title] = useState('')
-
-    const handleTextFieldChanges = e => {
-        setTask_title(e.target.value)
-    }
-
-    const resetTextField = () => {
-        setTask_title('')
-    }
-
-    const isTaskInclude = () => {
-        return tasks.some(task => task.title === task_title)
-    }
-
-    const addTask = () => {
-        setTasks([...tasks, {
-            id: tasks.length + 1,
-            title: task_title,
-            doing: false,
-        }])
-        resetTextField()
-    }
-
-    const fields = [
-        { key: 'id', label: 'No', _style: { width: '10%', textAlign: 'center', backgroundColor: '#ffcccc'} },
-        { key: 'title', label: 'タイトル', _style: { width: '50%', textAlign: 'center', backgroundColor: '#ffcccc'} },
-        { key: 'delete', label: '削除', _style: { width: '10%', textAlign: 'center', backgroundColor: '#ffcccc'} }
-    ]
-
-    const getBadge = (status)=>{
-        switch (status) {
-            case 'Active': return 'success'
-            case 'Inactive': return 'secondary'
-            case 'Pending': return 'warning'
-            case 'Banned': return 'danger'
-            default: return 'primary'
-        }
-    }
-    
-    const deleteRow = (item) => {
-        setTasks(tasks.filter(row => row.id !== item.id))
-    }
-
+    const [activeIndex, setActiveIndex] = useState(0)
 
     console.log("return");
-    console.log(tasks);
 
     return (
-        <React.Fragment>
+        <React.Fragment className="detail">
             <CContainer className="py-2">
                 <CRow>
-                    <CCol lg="12" className="bg-secondary py-3">
-                        作成中
+                    <CCol xs="3"></CCol>
+                    <CCol xs="6" className="bg-secondary py-3">
+                        <CCarousel activeIndex={activeIndex}>
+                            <CCarouselIndicators/>
+                            <CCarouselInner>
+                            <CCarouselItem>
+                                <img className="img d-block w-100" src={`${process.env.PUBLIC_URL}/carrot.png`} alt="carrot"/>
+                                <CCarouselCaption><h3>にんじん</h3><p>carrot</p></CCarouselCaption>
+                            </CCarouselItem>
+                            <CCarouselItem>
+                                <img className="img d-block w-100" src={`${process.env.PUBLIC_URL}/greenPepper.png`} alt="greenPepper"/>
+                                <CCarouselCaption><h3>ピーマン</h3><p>green pepper</p></CCarouselCaption>
+                            </CCarouselItem>
+                            <CCarouselItem>
+                                <img className="img d-block w-100" src={`${process.env.PUBLIC_URL}/nagasawa.png`} alt="nagasawa"/>
+                                <CCarouselCaption><h3>玉ねぎ</h3><p>nagasawa</p></CCarouselCaption>
+                            </CCarouselItem>
+                            <CCarouselItem>
+                                <img className="img d-block w-100" src={`${process.env.PUBLIC_URL}/potato.png`} alt="potato"/>
+                                <CCarouselCaption><h3>じゃがいも</h3><p>potato</p></CCarouselCaption>
+                            </CCarouselItem>
+                            </CCarouselInner>
+                            <CCarouselControl direction="prev"/>
+                            <CCarouselControl direction="next"/>
+                        </CCarousel>
                     </CCol>
+                    <CCol xs="3"></CCol>
                 </CRow>
             </CContainer>
         </React.Fragment>
