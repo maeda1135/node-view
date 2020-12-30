d819c3233b1c8eb7380429236642d5502a583b13
maeda <hiro92196@gmail.com>
2020-12-30 12:04:10 +0900

    xs 追加

--- src/components/Header.js
+++ src/components/Header.js
@@ -35,26 +35,26 @@ export const Header = () => {
         <React.Fragment>
             <CContainer className="py-3">
                 <CRow>
-                    <CCol lg="12" md="12" sm="12" className="bg-success py-3">
+                    <CCol xs="12" lg="12" md="12" sm="12" className="bg-success py-3">
                         <CContainer>
                             <CRow>
-                                <CCol lg="1" md="1" sm="1">
+                                <CCol xs="1" lg="1" md="1" sm="1">
                                     <Avatar alt="user" src={`${process.env.PUBLIC_URL}/user.png`} 
                                         onClick={toggle} className="avatar"/>
                                 </CCol>
-                                <CCol lg="9" md="9" sm="9">
+                                <CCol xs="9" lg="9" md="9" sm="9">
                                     <label className="userLabel">ユーザー名: <label className="username" onClick={toggle}>玉ねぎマン</label></label>
                                 </CCol>
-                                <CCol lg="2" md="2" sm="2">
+                                <CCol xs="2" lg="2" md="2" sm="2">
                                     <Link to="/"><CButton color="primary" className="homeButton">ホーム</CButton></Link>
                                     <Link to="/detail"><CButton color="warning">詳細</CButton></Link>
                                 </CCol>
                             </CRow>
                             <CRow>
-                                <CCol lg="6" md="6" sm="6">
+                                <CCol xs="6" lg="6" md="6" sm="6">
                                     <label className="appliLabel">TODO Appli</label>
                                 </CCol>
-                                <CCol lg="6" md="6" sm="6">
+                                <CCol xs="6" lg="6" md="6" sm="6">
                                     {/* <Link to="/detail"><Button variant="contained" color="primary">詳細</Button></Link> */}
                                 </CCol>
                             </CRow>
@@ -70,7 +70,7 @@ export const Header = () => {
                 <CModalBody style={{textAlign:'center'}}>
                     <CContainer>
                         <CRow>
-                            <CCol lg="12" md="12" sm="12">
+                            <CCol xs="12" lg="12" md="12" sm="12">
                                 <img alt="user" src={`${process.env.PUBLIC_URL}/user.png`} />
                             </CCol>
                         </CRow>
